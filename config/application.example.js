e6b6728c836e0d37da24085fd56942c0f030c0cd
Bill Patrianakos <bill@billpatrianakos.me>
2019-08-19 11:12:32 -0500

    Create example config file.

---
+++ config/application.example.js
@@ -0,0 +1,11 @@
+// Application configuration
+
+module.exports = {
+  development: {
+    api_key: '1d4859f8-8e90-4cfd-b4bc-d722380c489e',
+  },
+
+  production: {
+    api_key: 'CHANGE_ME', // Run `./system/keygen` from project root to generate a UUID to be used as an API key
+  }
+}
