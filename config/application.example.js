2d83ce190ede524ae411f9266e319fc4b78e86a4
Bill Patrianakos <bill@billpatrianakos.me>
2019-08-19 14:51:04 -0500

    Create knexfile and make config file more robust.

--- config/application.example.js
+++ config/application.example.js
@@ -1,14 +1,56 @@
 // Application configuration
 // =========================
-// IMPORTANT: Rename this file to `application.js`
-// and add your production values before deploying.
+
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 
 module.exports = {
-  development: {
+  defaults: {
+    port: process.env.PORT || 9000,
+    session: {
+      cookie: { maxAge: 86400000 },
+      name: 'pisense.sid',
+      resave: false,
+      rolling: true,
+      saveUninitialized: false,
+      secret: 'f6f15927-dd1b-485f-8207-bdf41bb81b0e!',
+      unset: 'destroy'
+    },
     api_key: '1d4859f8-8e90-4cfd-b4bc-d722380c489e',
   },
 
+  development: {
+    database: {
+      client: '', // Left blank on purpose
+      connection: {} // Left blank on purpose
+    }
+  },
+
+  // CHANGE ALL OF THESE BEFORE DEPLOYING
+  // ------------------------------------
+  // Change these configs as you wish. Not all options are
+  // required. Where each is used is specified beside the parameter.
   production: {
+    defaultUsername: 'CHANGE_ME', // Used in database seed file to populate your username
+    defaultPassword: 'CHANGE_ME', // Used in database seed file to populate your username
+    database: {
+      client: 'postgresql', // Used in knexfile. Change based on your database (postgres, pg, mysql, etc.)
+      connection: { // This entire object is used in knexfile.js. Modify this based on the database of your choosing
+        host: 'localhost',
+        database: 'CHANGE_ME',
+        port: 5432, // Not always needed
+        user:     'CHANGE_ME',
+        password: 'CHANGE_ME'
+      }
+    },
+    session: { // Used in server/index.js. Controls session middleware
+      secret: 'CHANGE_ME', // Choose a long secure string here
+      store: new RedisStore({ // You'll need Redis installed and running to use RedisStore as your session store
+        host: '127.0.0.1',
+        port: 6379,
+        ttl: 7200000 // 2 hours
+      })
+    },
     api_key: 'CHANGE_ME', // Run `./system/keygen` from project root to generate a UUID to be used as an API key
   }
-}
+};
---
+++ knexfile.js
@@ -0,0 +1,35 @@
+const _       = require('lodash');
+const config  = _.merge(require('./config/application').defaults, require('./config/application')[process.env.NODE_ENV || 'development']);
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    migrations: {
+      directory: './config/db/migrations'
+    },
+    seeds: {
+      directory: './config/db/seeds/dev'
+    }
+  },
+
+  production: {
+    client: config.database.client,
+    connection: config.database.connection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './config/db/migrations'
+    },
+    seeds: {
+      directory: './config/db/seeds/production'
+    }
+  }
+
+};
