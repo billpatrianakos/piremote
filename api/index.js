fe246db8546589fdce2fa5ddeab913c6eb615d85
Bill Patrianakos <bill@billpatrianakos.me>
2019-08-19 16:24:53 -0500

    Add logic for sending remote commands on CLI.

--- api/index.js
+++ api/index.js
@@ -1,1 +1,68 @@
+// Main server setup and startup script
+// ====================================
 
+// Require dependencies
+// --------------------
+const _           = require('lodash');
+const config      = _.merge(require('../config/application').defaults, require('../config/application')[process.env.NODE_ENV || 'development']);
+const express     = require('express');
+const app         = express();
+const path        = require('path');
+const morgan      = require('morgan');
+const session     = require('express-session');
+const bodyParser  = require('body-parser');
+const flash       = require('connect-flash');
+const isProd      = function() { return _.includes(['production'], process.env.NODE_ENV); };
+const fs          = require('fs');
+
+
+// Require environment-specific dependencies
+// -----------------------------------------
+if (process.env.NODE_ENV === 'production') {
+  const RedisStore = require('connect-redis')(session);
+}
+
+
+// Configure settings and middleware
+// ---------------------------------
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('trust proxy', isProd() ? 1 : 0);
+app.set('config', config);
+app.use(morgan(isProd() ? 'combined' : 'dev', {
+  stream: isProd() ? fs.createWriteStream(__dirname + '/log/access.log', { flags: 'a' }) : process.stdout,
+  skip: (req, res) => { return isProd() && res.statusCode < 400 }
+}));
+app.use(session(config.session));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use('csurf'); // TODO: Implement CSURF
+app.use(flash());
+if (!isProd()) app.use(express.static(path.join(__dirname, '/../public')));
+
+
+// Load up our routes
+// ------------------
+require('./routes')(app);
+app.use('/', require('./routes/main'));
+
+
+// 404 handler
+// -----------
+app.use((req, res, next) => {
+  res.status(404).render('404', { error: 'Page not found' });
+});
+
+
+// 500 handler
+// -----------
+app.use((err, req, res, next) => {
+  res.status(500).render('500', { error: err });
+});
+
+
+// Start the server
+// ----------------
+let server = app.listen(config.port || 9000, () => {
+  console.log(`PiRemote server listening on localhost:${server.address().port} in ${process.env.NODE_ENV} mode`);
+});
--- api/routes/remote.js
+++ api/routes/remote.js
@@ -1,4 +1,5 @@
-
+const { exec } = require('child_process'),
+      commands = require('../../config/commands');
 
 RemoteController.route('/?')
   // GET /remote
@@ -13,13 +14,38 @@ RemoteController.route('/command/?')
   // -------------
   // Send a JSON request to command API
   .post((req, res, next) => {
-    let command = req.body.command;
+    let command  = req.body.command;
+    let response = '...';
 
-    // Handle single key and multi-key commands
-    if (!command.length) {
-      // handle single key commands here
+    if (commands[command]) {
+      // Handle single key and multi-key commands
+      if (!command.length) {
+        // handle single key commands
+        exec(`../../system/remote ${command.keyName} ${command.holdTime}`, (error, stdout, stderr) => {
+          if (error) {
+            return next(error);
+          }
+          response += stdout;
+          response += stderr;
+        });
+
+        res.status(200).json({ status: 'ok', response: response });
+      } else {
+        // handle multi-key press commands
+        response = '...';
+        command.forEach((cmd) => {
+          exec(`../../system/remote ${command.keyName} ${command.holdTime}`, (error, stdout, stderr) => {
+            if (error) {
+              return next(error);
+            }
+            response += stdout;
+            response += stderr;
+          });
+        });
+        res.status(200).json({ status: 'ok', response: response });
+      }
     } else {
-      // handle multi-key press commands here
+      next('Unrecognized remote command. Make sure to send a remote command that matches the name of a command object key.')
     }
   });
 
