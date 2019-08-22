0db0d49377292a2503ae973e2a0713d07b95d935
Bill Patrianakos <bill@billpatrianakos.me>
2019-08-22 16:28:48 -0500

    Add script to call remote API.

---
+++ public/js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  // Runs when button is pressed on virtual remote interface
+  $('.remote-button').click(function(e) {
+    e.preventDefault();
+
+    var command = $(this).data('command');
+
+    // Send POST request to commands API
+    $.ajax({
+      method: 'POST',
+      url: '/api/remote/command',
+      data: {
+        api_key: apiKey,
+        command: command
+      }
+    })
+    .done(function(result) {
+      if (result.status === 'ok') {
+        alert('All good');
+      } else {
+        alert(result.response);
+      }
+    });
+  });
+});
