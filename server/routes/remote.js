ac5bb5ff31520a38eebe16e0ccd5e79a7f7544bb
Bill <billpatrianakos@users.noreply.github.com>
2019-08-28 08:34:36 -0500

    Fix issue where multi key presses did not work.

--- server/routes/remote.js
+++ server/routes/remote.js
@@ -42,7 +42,7 @@ RemoteController.route('/command/?')
         // handle multi-key press commands
         let timesRun = 0
         commands[command].forEach((cmd) => {
-          exec(`${__dirname}/../../system/remote hold ${commands[command].keyName} ${commands[command].holdTime}`, (error, stdout, stderr) => {
+          exec(`${__dirname}/../../system/remote hold ${cmd.keyName} ${cmd.holdTime}`, (error, stdout, stderr) => {
             if (error) errors.push(error);
 
             response += stdout;
